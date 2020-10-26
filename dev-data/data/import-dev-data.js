85c6a9e6c5346ff22a1d416b51225471e35f8784
Elmi Rabelo <rabeloelmi@gmail.com>
2020-10-25 21:11:55 -0300

    Create a script to export data to the databse

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: `${__dirname}/../../config.env` });
+
+const { env } = process;
+
+const DB = env.DATABASE.replace('<PASSWORD>', env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Database conection established'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data loaded');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data deleted');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
