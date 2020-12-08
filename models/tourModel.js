a85525256e77e0bd170225b611e6516caf2ea9a5
Elmi Rabelo <rabeloelmi@gmail.com>
2020-12-07 21:36:51 -0300

    Add virtual properties to tourController

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,56 +1,70 @@
 const { Schema, model } = require('mongoose');
 
-const tourSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Name must have a value'],
-    trim: true,
-    unique: true
+const tourSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name must have a value'],
+      trim: true,
+      unique: true
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Tour must have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Tour must have a group size']
+    },
+    difficulty: {
+      type: String
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'Tour must have a price']
+    },
+    summary: {
+      type: String,
+      require: [true, 'Tour must have a summery'],
+      trim: true
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    startDates: [Date]
   },
-  duration: {
-    type: Number,
-    required: [true, 'Tour must have a duration']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Tour must have a group size']
-  },
-  difficulty: {
-    type: String
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'Tour must have a price']
-  },
-  summary: {
-    type: String,
-    require: [true, 'Tour must have a summery'],
-    trim: true
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'Tour must have a cover image']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  startDates: [Date]
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return Math.round(this.duration / 7);
 });
+
+// tourSchema.pre('save', function() {
+
+// });
 
 const Tour = model('Tour', tourSchema);
 
