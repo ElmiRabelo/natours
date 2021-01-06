23b37543354fefe7c26366c2e94e96f86a890db6
Elmi Rabelo <rabeloelmi@gmail.com>
2021-01-05 22:04:27 -0300

    Add some security measures

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorController = require('./controllers/errorController');
@@ -9,12 +11,25 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Set Security HTTP headers
+app.use(helmet());
+
+// limiting requests from same ip address
+const limiter = rateLimit({
+  max: 150,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
